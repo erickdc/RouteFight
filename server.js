d8d480a3e0926e985afa3903413afd9d24a28df5
erickdc <erickson_523@hotmail.com>
2016-03-20 12:26:26 -0600

    changes to server

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ var port = Number(process.env.PORT || 3000);
 
 server.connection({ port: port });
 
+server.register(require('inert'), (err) => {
+    if (err) {
+        throw err;
+    }
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            directory: {
+                path: './client/src',
+                listing: false,
+                index: true
+            }
+        },
+        config:{
+            state:{
+                parse: true,
+                failAction:'ignore'
+            },
+            auth:false
+        }
+    });
+});
+
 server.start((err) => {
 
     if (err) {
